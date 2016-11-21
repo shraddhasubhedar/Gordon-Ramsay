bb79234d1d45cab3ab69d6009445c9fef8d3e3f8
Kevin Ng <kevin.ng.ngk3@gmail.com>
2016-11-21 18:30:58 -0500

    Update Inventory.java
    added comments

--- GordonRamSDD/app/src/main/java/com/example/gordonramsdd/Inventory.java
+++ GordonRamSDD/app/src/main/java/com/example/gordonramsdd/Inventory.java
@@ -6,19 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+// Class and Activity used to redirect users depending on the buttons they press
 public class Inventory extends Activity {
 
     @Override
+    // Function that is called when the activity is created
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
     }
 
+    // Redirects to CreateInventory
     protected void create_inventory(View view) {
         Intent intent = new Intent(this, CreateInventory.class);
         startActivity(intent);
     }
 
+    // Redirects to ExistInventory
     protected void exist_inventory(View view) {
         Intent intent = new Intent(this, ExistInventory.class);
         startActivity(intent);
